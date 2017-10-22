af7b9a02f0af1c53aefdc56777c8113a1ac4f5f0
Frye <klayton.alan@gmail.com>
2017-10-22 18:05:09 +0500

    restore

--- src/index.js
+++ src/index.js
@@ -1,31 +1,37 @@
-import './main.css'
+import './index.css';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import thunk from 'redux-thunk'
-import {syncHistoryWithStore} from 'react-router-redux'
-import {Router, Route, browserHistory} from 'react-router'
-import {Provider} from 'react-redux'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import reducers from 'reducers'
-import Layout from 'containers/layout'
-import Phones from 'containers/phones'
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers, composeWithDevTools(
-    applyMiddleware(thunk)
-))
+import thunk from 'redux-thunk';
 
-const history = syncHistoryWithStore(browserHistory, store)
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router-dom';
+import { ConnectedRouter,  routerMiddleware } from 'react-router-redux';
+import registerServiceWorker from './registerServiceWorker';
+
+import reducers from 'reducers';
+import Layout from 'containers/layout';
+import Phones from 'containers/phones';
+
+const history = createHistory();
+const middleware = routerMiddleware(history);
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk, middleware)));
 
 ReactDOM.render(
     <Provider store={store}>
-        <Router history={history}>
-            <Route component={Layout}>
+        <ConnectedRouter history={history}>
+            {/*Содержит sidebar и место для контента*/}
+            <Layout>
                 <Route path='/' component={Phones} />
-            </Route>
-        </Router>
+            </Layout>
+        </ConnectedRouter>
     </Provider>,
     document.getElementById('root')
 );
+
+registerServiceWorker();
