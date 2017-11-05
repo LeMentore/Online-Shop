b1a82f87f87cfdf59fa22487e64438b752d9a465
Frye <klayton.alan@gmail.com>
2017-11-05 18:58:50 +0500

    created filter for search in phones

--- src/reducers/phonesPage.js
+++ src/reducers/phonesPage.js
@@ -2,11 +2,13 @@ import * as R from 'ramda';
 
 import {
     FETCH_PHONES_SUCCESS,
-    LOAD_MORE_PHONES_SUCCESS
+    LOAD_MORE_PHONES_SUCCESS,
+    SEARCH_PHONE
 } from 'actionTypes';
 
 const initialState = {
-    ids: []
+    ids: [],
+    search: ''
 };
 
 export default (state = initialState, {type, payload}) => {
@@ -21,6 +23,10 @@ export default (state = initialState, {type, payload}) => {
             console.log(state.ids);
             return R.merge(state, {
                ids: R.concat(state.ids, ids)
+            });
+        case SEARCH_PHONE:
+            return R.merge(state, {
+                search: payload
             });
         default:
             return state;
