09337bb16b75054d8322f7f2ea32f42ed1a7676f
Frye <klayton.alan@gmail.com>
2017-11-12 23:06:19 +0500

    test api

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,29 @@
 import * as R from 'ramda';
-import phones from './mockPhones';
+import request from 'superagent';
+
 import categories from './mockCategories';
 
 export const fetchPhones = async () => {
-    return new Promise((resolve, reject) => {
-        resolve(phones);
-    })
+    const {body} = await request.get(
+        'http://www.mocky.io/v2/5a088bc33200003e06137ff5'
+    );
+    return body.phones;
 };
 
 export const loadMorePhones = async ({offset}) => {
-    return new Promise((resolve, reject) => {
-        resolve(phones);
-    })
+    const {body} = await request.get(
+        'http://www.mocky.io/v2/5a088bc33200003e06137ff5'
+    );
+    return body.phones;
 };
 
 export const fetchPhoneById = async id => {
-    return new Promise((resolve, reject) => {
-        const phone = R.find(R.propEq('id', id), phones);
-        resolve(phone);
-    })
+    const {body} = await request.get(
+        'http://www.mocky.io/v2/5a088bc33200003e06137ff5'
+    );
+
+    const phone = R.find(R.propEq('id', id), body.phones);
+    return phone;
 };
 
 export const fetchCategories = async () => {
